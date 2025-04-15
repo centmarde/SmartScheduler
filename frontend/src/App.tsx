fd97e718f8d488200f2f6875699772d4be91fbc5
Marde Saint <159101935+centmarde@users.noreply.github.com>
2025-04-16 04:19:59 +0800

    feat: Implement new UI components and theme integration
    - Added InsideNavbar and Navbar components for navigation.
    - Created reusable UI components: Button, Card, and Table.
    - Developed Landing and Table pages with responsive layouts.
    - Integrated theme context for consistent styling across components.
    - Established a color palette and custom styles for buttons and text.
    - Enhanced table functionality with dynamic schedule data.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-
-import ButtonUsage from './ui/buttons.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/Landing';
+import TeacherSchedule from './pages/Table';
+import { ThemeProvider } from './theme/theme';
+import './index.css'
 
 function App() {
   return (
-    <>
-      <ButtonUsage />
-    </>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/table" element={<TeacherSchedule />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
